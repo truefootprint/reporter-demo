eb11e57a98c94864d88812afc99bbf6f8d3623a5
Chris Patuzzo <chris@patuzzo.co.uk>
2019-05-18 21:17:20 +0100

    Allow the app to be added as a ‘home screen app’

--- components/layout/Layout.jsx
+++ components/layout/Layout.jsx
@@ -5,7 +5,16 @@ import "./typography.scss";
 
 const Layout = ({ children }) => <>
   <Head>
+    <title>TrueFootprint</title>
+
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700" rel="stylesheet" />
+    <link href="/static/images/apple-touch-icon.png" rel="apple-touch-icon" />
+
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <meta name="mobile-web-app-capable" content="yes" />
+    <meta name="apple-mobile-web-app-capable" content="yes" />
+    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
   </Head>
 
   {children}
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,21 @@
 import Layout from "../components/layout";
 import Loading from "../components/loading";
+import Palette from "../components/palette";
+import css from "./styles.scss";
 
 const Index = () => (
   <Layout>
     <Loading />
-    <p>Hello, world!</p>
+    <p className={css.instructions}>
+      Please click on the icon in the bottom-middle of your iPhone screen to add this app to your home screen.
+    </p>
+
+    <div className={css.restart}>
+      <a href="/">Restart App</a>
+    </div>
+
+    <p className={css.color_palette}>Color palette</p>
+    <Palette />
   </Layout>
 );
 
