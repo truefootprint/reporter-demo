42bf9121dd1ace2f6f8543742e2f4cdc984383a5
Chris Patuzzo <chris@patuzzo.co.uk>
2019-05-25 14:42:34 +0100

    Add a mechanism for changing project/language
    On the ‘Thank you’ screen, when you click on the
    TrueFootprint logo to restart the app, instead
    show a dropdown that offers a choice of project
    and language.
    
    When selected, set the project/language state and
    restart the app. The content is then sourced from
    a static/content.js file which is passed into each
    of the pages.

--- components/thanks/Thanks.jsx
+++ components/thanks/Thanks.jsx
@@ -1,9 +1,20 @@
+import { useRef } from "react";
 import Spinner from "../spinner";
 import Logo from "../logo";
 import css from "./styles.scss";
 
-const Thanks = ({ setPage, setLoading }) => {
+const Thanks = ({ setPage, setLoading, setProject, setLanguage }) => {
+  const projectMenu = useRef(null);
+
+  const openMenu = () => {
+    projectMenu.current.focus();
+  };
+
   const restartApp = () => {
+    const [project, language] = projectMenu.current.value.split(",");
+
+    setProject(project);
+    setLanguage(language);
     setPage("overview");
     setLoading(true);
   };
@@ -22,10 +33,17 @@ const Thanks = ({ setPage, setLoading }) => {
         We look forward to your next update in two weeks time.
       </p>
 
-      <a className={css.logo} onClick={restartApp}>
+      <a className={css.logo} onClick={openMenu}>
         <Logo variant="stackedText" />
       </a>
     </div>
+
+    <select ref={projectMenu} className={css.hidden} onChange={restartApp}>
+      <option value="school,english">School: English</option>
+      <option value="school,swahili">School: Swahili</option>
+      <option value="roads,english">Roads: English</option>
+      <option value="roads,swahili">Roads: Swahili</option>
+    </select>
   </>;
 };
 
--- pages/index.jsx
+++ pages/index.jsx
@@ -4,10 +4,13 @@ import Loading from "../components/loading";
 import Overview from "../components/overview";
 import Form from "../components/form";
 import Thanks from "../components/thanks";
+import Content from "../static/content.js";
 
 const Index = () => {
-  const [page, setPage] = useState("overview");
-  const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState("thanks");
+  const [loading, setLoading] = useState(false);
+  const [project, setProject] = useState("school");
+  const [language, setLanguage] = useState("english");
 
   const setPageAndScroll = (page) => {
     window.scrollTo(0, 0);
@@ -15,13 +18,16 @@ const Index = () => {
     setLoading(false);
   };
 
+  const content = Content[project][language];
+
   return (
     <Layout>
+      <p>{content.hello}</p>
       {loading && <Loading />}
 
-      {page === "overview" && <Overview setPage={setPageAndScroll} />}
-      {page === "form" && <Form setPage={setPageAndScroll} />}
-      {page === "thanks" && <Thanks setPage={setPageAndScroll} setLoading={setLoading} />}
+      {page === "overview" && <Overview setPage={setPageAndScroll} content={content} />}
+      {page === "form" && <Form setPage={setPageAndScroll} content={content} />}
+      {page === "thanks" && <Thanks setPage={setPageAndScroll} setLoading={setLoading} content={content} />}
     </Layout>
   );
 };
